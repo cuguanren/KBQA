bc24fca9f4499f71d5883c307489b757d3c3dafa
cuguanren <102279597+cuguanren@users.noreply.github.com>
2022-07-16 10:47:28 +0800

    Update 建立图谱.py

--- 建立图谱.py
+++ 建立图谱.py
@@ -6,7 +6,7 @@ g=Graph('http://localhost:7474', user='neo4j', password='neo4j')
 #创建config以及db.cfg用来存储信息
 
 
-with open('C:\\Users\\cuguanren\pytorch实践项目\\基于知识图谱的电影问答系统\\data\\genre.csv','r',encoding='utf-8') as f:
+with open('C:\\Users\\data\\genre.csv','r',encoding='utf-8') as f:
     #数据集除了第一行代表属性外，第一列为实体1，第二列为实体2，第三列是两者英文关系，第四列为两者中文关系
     reader=csv.reader(f)
     for item in reader:
@@ -16,7 +16,7 @@ with open('C:\\Users\\cuguanren\pytorch实践项目\\基于知识图谱的电影
         #print("当前行数：",reader.line_num,"当前内容",item)
         test_node_1=Node("Genre",id=item[0],name=item[1])
         g.merge(test_node_1, "Genre", "id")
-with open('C:\\Users\\cuguanren\pytorch实践项目\\基于知识图谱的电影问答系统\\data\\movie.csv','r',encoding='utf-8') as f:
+with open('C:\\Users\\data\\movie.csv','r',encoding='utf-8') as f:
     reader=csv.reader(f)
     for item in reader:
         #第一行的标签不是需要的内容，line_num表示文件的第几行
@@ -25,7 +25,7 @@ with open('C:\\Users\\cuguanren\pytorch实践项目\\基于知识图谱的电影
         print("当前行数：",reader.line_num,"当前内容",item)
         test_node_1=Node("Movie",id=item[0],title=item[1],introduction=item[2],rating=item[3],releasedate=item[4])
         g.merge(test_node_1, "Movie", "id")
-with open('C:\\Users\\cuguanren\pytorch实践项目\\基于知识图谱的电影问答系统\\data\\person.csv','r',encoding='utf-8') as f:
+with open('C:\\Users\\data\\person.csv','r',encoding='utf-8') as f:
     #数据集除了第一行代表属性外，第一列为实体1，第二列为实体2，第三列是两者英文关系，第四列为两者中文关系
     reader=csv.reader(f)
     for item in reader:
@@ -38,7 +38,7 @@ with open('C:\\Users\\cuguanren\pytorch实践项目\\基于知识图谱的电影
 matcher = NodeMatcher(g)
 findnode = matcher.match('Person', id='9550').first()
 print(findnode)
-with open('C:\\Users\\cuguanren\pytorch实践项目\\基于知识图谱的电影问答系统\\data\\person_to_movie.csv','r',encoding='utf-8') as f:
+with open('C:\\Users\\data\\person_to_movie.csv','r',encoding='utf-8') as f:
     #数据集除了第一行代表属性外，第一列为实体1，第二列为实体2，第三列是两者英文关系，第四列为两者中文关系
     reader=csv.reader(f)
     for item in reader:
@@ -50,7 +50,7 @@ with open('C:\\Users\\cuguanren\pytorch实践项目\\基于知识图谱的电影
         endnode = matcher.match('Movie', id=item[1]).first()
         relationships = Relationship(findnode, '饰演', endnode)
         g.merge(relationships, "", "id")
-with open('C:\\Users\\cuguanren\pytorch实践项目\\基于知识图谱的电影问答系统\\data\\movie_to_genre.csv', 'r', encoding='utf-8') as f:
+with open('C:\\Users\\data\\movie_to_genre.csv', 'r', encoding='utf-8') as f:
     #数据集除了第一行代表属性外，第一列为实体1，第二列为实体2，第三列是两者英文关系，第四列为两者中文关系
     reader=csv.reader(f)
     for item in reader:
